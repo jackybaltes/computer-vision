86a3a9e8072848504771d077d83f593564711591
Nikola Bellendier <fosmidt14@outlook.com>
2022-02-12 12:59:39 +0800

    deploy Next.js to gh-pages

---
+++ _next/static/XckDoU3gHdcwsJbOP98k3/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-9b95736b0d040e44.js"],"/_error":["static/chunks/pages/_error-0cb2bff341a47607.js"],"/about":["static/chunks/pages/about-c09da8d167b9f89d.js"],sortedPages:["/","/_app","/_error","/about"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
